c8fb3c1938465ec62e705d56cf172963509bc860
deanovision <rodeanfraser@gmail.com>
2020-05-24 19:16:35 -0400

    nodemailer

--- server/index.js
+++ server/index.js
@@ -1,11 +1,45 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.post("/", (req, res) => {
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    clientId: "xxxxxxxxxxx",
+    clientSecret: "xxxxxxxxxx",
+  },
+});
+
+transporter.set("oauth2_provision_cb", (user, renew, callback) => {
+  let accessToken = userTokens[user];
+  if (!accessToken) {
+    return callback(new Error("Unknown user"));
+  } else {
+    return callback(null, accessToken);
+  }
+});
+
+server.get("/", (req, res) => {
   console.log(req.body);
   //   res.location("back");
-  res.redirect("back");
+  //   transporter.sendMail({
+  //     from: "xxxxxxxxxxxx",
+  //     to: "xxxxxxxxxxxxx",
+  //     subject: "Message",
+  //     text: "I hope this message gets through!",
+  //     auth: {
+  //       user: "xxxxxxxxxxxx",
+  // refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
+  // accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
+  // expires: 1484314697598
+  // },
+  //   });
+  res.send("connected");
 });
 
 module.exports = server;
